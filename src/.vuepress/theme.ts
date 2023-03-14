3629addaa23bcdbdd40049d1ec46c4ca1c550b88
biless <biless@qq.com>
2023-03-14 16:39:54 +0800

    docs: 添加Android编译文档

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,11 +3,11 @@ import { zhNavbar } from "./navbar/index.js";
 import { zhSidebar } from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  hostname: "https://flutterplayer.github.io/",
 
   author: {
-    name: "Zwfon",
-    url: "https://mrhope.site",
+    name: "fplayer",
+    url: "https://flutterplayer.github.io/",
   },
 
   iconAssets: "iconfont",
@@ -103,62 +103,5 @@ export default hopeTheme({
       vPre: true,
       vuePlayground: true,
     },
-
-    // uncomment these if you want a pwa
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cachePic: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
   },
 });
--- src/core/android.md
+++ src/core/android.md
@@ -1,3 +1,61 @@
 ---
 title: Android内核编译
 ---
+# 提示
+目前所有的环境均在Mac上编译如果是其他平台暂时不提供问题自行研究
+
+## 前期准备工作
+1. 先装基础环境
+
+```bash
+brew install git
+brew install yasm
+brew install python
+```
+
+2. 安装ndk
+
+确保安装android studio之后 可以使用如下脚本进行下载
+```bash
+sdkmanager --install "ndk;21.1.6352462" "cmake;3.10.2.4988404"
+```
+或者直接在UI界面进行下载对应版本
+
+2. 设置环境变量
+
+```bash
+export ANDROID_NDK=$ANDROID_HOME/ndk/21.1.6352462/
+```
+注意在这个之前你需要将 ANDROID_HOME 的环境变量配置好
+
+
+## 编译
+1. 下载源码
+```bash
+git clone https://github.com/FlutterPlayer/ijkplayer.git ijkplayer-android
+cd ijkplayer-android
+```
+
+2. 初始化工程以及下载依赖源码
+```bash
+bash init-android.sh
+bash init/init-android-boringssl.sh
+bash init/init-android-libsrt.sh
+```
+
+3. 编译FFMpeg
+```bash
+cd android/contrib
+bash compile-boringssl.sh all
+bash compile-ffmpeg.sh all
+```
+
+4. 编译ijkplayer
+```bash
+cd ../ijkplayer
+./gradlew build
+./gradlew :fijkplayer-full:assembleRelease
+```
+
+## 产物
+在目录下会产生fijkplayer-full-release.aar文件引入即可使用
